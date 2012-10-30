388c0a62a2ab5a91be030e6acc7d11f9b965ed2f
Luis Verduzco <lgverduzco@gmail.com>
2012-10-30 11:43:13 -0600

    Falta arreglar conectores de for, if y while
    Avance 5

--- T-Rex Grafico/trex.js
+++ T-Rex Grafico/trex.js
@@ -46,111 +46,149 @@ function click_while(){
   console.log("while");
 }
 
-function agregar_estatuto(elemento) {
+function agregar_estatuto(elemento, tipo) {
     var x;
 	var est_text=prompt("Agrega estatuto","Ejem: Fin");
 	
-	if (est_text!=""){
+	if (est_text!="" && tipo!="end" && tipo!=mid){
 	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="estatuto"> <span>' + est_text + '</span></div><div class="conector"></div></div>';
 	  $(elemento).after(x);  
+	}
+	else if(tipo=="mid"){
+		x='<div class="elemento" onclick="agregar_elem(this, "mid")"><div class="estatuto"> <span>' + est_text + '</span></div><div class="conector"></div></div>';
+		$(elemento).after(x);  
+	}
+	else if(tipo=="end"){
+		x='<div class="elemento" onclick="agregar_elem(this)"><div class="estatuto"> <span>' + est_text + '</span></div><div class="conector"></div></div>';
+		$(elemento).parent().after(x);  
 	}
 	else{
 		alert("No declaraste un estatuto");
 	}
 }
 
-function agregar_variable(elemento) {
+function agregar_variable(elemento, tipo) {
 	var x;
 	var var_text=prompt("Agrega tipo, nombre y valor de la variable","Ejem: int x = 3");
 	
-	if (var_text!=""){
+	if (var_text!="" && tipo!="end" && tipo!=mid){
 	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="variable"> <span>' + var_text + '</span></div><div class="conector"></div></div>';
 	  $(elemento).after(x);  
 	}
+	else if(tipo=="mid"){
+		x='<div class="elemento" onclick="agregar_elem(this, "mid")"><div class="variable"> <span>' + var_text + '</span></div><div class="conector"></div></div>';
+		$(elemento).after(x);  
+	}
+	else if(tipo=="end"){
+		x='<div class="elemento" onclick="agregar_elem(this)"><div class="variable"> <span>' + var_text + '</span></div><div class="conector"></div></div>';
+		$(elemento).parent().after(x);  
+	}
 	else{
-		alert("No declaraste un elemento");
+		alert("No declaraste una variable");
 	}
 }
 
-function agregar_if(elemento) {
+function agregar_if(elemento, tipo) {
 	var x;
 	var if_text=prompt("Agrega condicion","Ejem: x > y");
 	
-	if (if_text!=""){
+	if (if_text!="" && tipo!="end"){
 	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="if"> <span class="textoIf">' + if_text + '</span></div><div class="conector"></div></div>';
 	  $(elemento).after(x);  
+	}
+	else if(tipo=="end"){
+		x='<div class="elemento" onclick="agregar_elem(this)"><div class="if"> <span class="textoIf">' + if_text + '</span></div><div class="conector"></div></div>';
+		$(elemento).parent().after(x);  
 	}
 	else{
 		alert("No declaraste un if");
 	}
 }
 
-function agregar_operacion(elemento) {
+function agregar_operacion(elemento, tipo) {
     var x;
 	var op_text=prompt("Agrega operacion","Ejem: x = x + 1");
 	
-	if (op_text!=""){
+	if (op_text!="" && tipo!="end"){
 	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="operacion"> <span>' + op_text + '</span></div><div class="conector"></div></div>';
 	  $(elemento).after(x);    
+	}
+	else if(tipo=="end"){
+		x='<div class="elemento" onclick="agregar_elem(this)"><div class="operacion"> <span>' + op_text + '</span></div><div class="conector"></div></div>';
+		$(elemento).parent().after(x);  
 	}
 	else{
 		alert("No declaraste una operacion");
 	}
 }
 
-function agregar_for(elemento) {
+function agregar_for(elemento, tipo) {
     var x;
 	var for_text=prompt("Agrega for","Ejem: i = 0; i < 10; i++");
 	
-	if (for_text!=""){
-	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="for"> <span>' + for_text + '</span></div><div class="conector"></div></div>';
+	if (for_text!="" && tipo!="end" && tipo!="mid"){
+	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="for"> <span>' + for_text + '</span></div><div class="conector"></div><div class="endfor">End For</div><p class="conector2"></p><div class="conector"></div></div>';
 	  $(elemento).after(x);    
+	}
+	else if(tipo=="mid"){
+		alert("No se pueden anidar ciclos");
+	}
+	else if(tipo=="end"){
+	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="for"> <span>' + for_text + '</span></div><div class="conector"></div><div class="endfor">End For</div><p class="conector2"></p><div class="conector"></div></div>';
+		$(elemento).parent().after(x);  
 	}
 	else{
 		alert("No declaraste un for");
 	}
 }
 
-function agregar_while(elemento) {
+function agregar_while(elemento, tipo) {
     var x;
 	var while_text=prompt("Agrega while","Ejem: x < 10");
 	
-	if (while_text!=""){
+	if (while_text!="" && tipo!="end" && tipo!="mid"){
 	  x='<div class="elemento" onclick="agregar_elem(this)"><div class="while"> <span>' + while_text + '</span></div><div class="conector"></div></div>';
 	  $(elemento).after(x);    
+	}
+	else if(tipo=="end"){
+	  	x='<div class="elemento" onclick="agregar_elem(this)"><div class="while"> <span>' + while_text + '</span></div><div class="conector"></div></div>';
+		$(elemento).parent().after(x);  
+	}
+	else if(tipo=="mid"){
+		alert("No se pueden anidar ciclos");
 	}
 	else{
 		alert("No declaraste un while");
 	}
 }
 
-function agregar_elem(elemento){
+function agregar_elem(elemento, tipo){
 	switch (elem_selec){
 	case 0:
 	  alert("Selecciona un simbolo antes de agregar");
 	  break;
 	case 1:
-	  agregar_estatuto(elemento);
+	  agregar_estatuto(elemento, tipo);
 	  elem_selec = 0;
 	  break;
 	case 2:
-	  agregar_variable(elemento)
+	  agregar_variable(elemento, tipo)
 	  elem_selec = 0;
 	  break;
 	case 3:
-	  agregar_if(elemento);
+	  agregar_if(elemento, tipo);
 	  elem_selec = 0;
 	  break;
 	case 4:
-	  agregar_operacion(elemento);
+	  agregar_operacion(elemento, tipo);
 	  elem_selec = 0;
 	  break;
 	case 5:
-	  agregar_for(elemento);
+	  agregar_for(elemento, tipo);
 	  elem_selec = 0;
 	  break;
 	case 6:
-	  agregar_while(elemento);
+	  agregar_while(elemento, tipo);
 	  elem_selec = 0;
 	  break;	  
 	}
