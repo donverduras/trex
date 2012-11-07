9f8c4e89a3d69c9793309234c0d022460eaf203f
Salvador Salazar <ssalazars@gmail.com>
2012-11-06 21:51:34 -0600

    Server-NodeJS
    Escritura a un archivo de texto desde el servidor

--- Flex_Bison/server.js
+++ Flex_Bison/server.js
@@ -1,28 +1,30 @@
 var http = require("http");
+var cont = 0;
+var string = "program prueba(){}";
 
 http.createServer(function (request, response) {
-   request.on("end", function () {
-      response.writeHead(200, {
-         'Content-Type': 'text/plain'
-      });
-      // Send data and end response.
-      response.end('Hello HTTP!');
-   });
+					request.on("end", function () {
+										response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+					response.end('Hello HTTP!');
+				 });
    
-   var exec = require('child_process').exec,
-    child;
-
-	child = exec('g++ Trex.tab.c lex.yy.c `pkg-config --cflags --libs glib-2.0` semantic_methods.c semantic_methods.h -ll -o Trex',
-  				function (error, stdout, stderr) {
-    				if(error==null){
-    					var execute = require('child_process').exec, trex;
-
-						trex = execute('./Trex test.txt',
-  									function (error, stdout, stderr) {
-    									if(error==null){
-    										console.log(stdout);
-    									}
-									});
-    				}
-				});
+   					var fs = require('fs');
+   					fs.writeFile('prueba.txt', string, function (err) {
+  																if (err) throw err;
+  																	console.log('It\'s saved!');
+																});
+   
+					var exec = require('child_process').exec, child;
+					child = exec('g++ Trex.tab.c lex.yy.c `pkg-config --cflags --libs glib-2.0` semantic_methods.c semantic_methods.h -ll -o Trex',
+  								 function (error, stdout, stderr) {
+    								var execute = require('child_process').exec, trex;
+									
+									trex = execute('./Trex test.txt', function (error, stdout, stderr) {
+    																	if(cont == 0){
+    																		console.log(stdout);
+    																		cont++;
+    																	}
+													});
+							});
 }).listen(8080);
